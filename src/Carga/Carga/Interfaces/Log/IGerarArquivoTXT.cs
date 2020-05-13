e128a9fb621b94081f9aabeabf3763bad3fd74d0
Washington <Washington@WASHINGTON-PC>
2020-05-13 09:32:35 -0300

    Add Interface IGerarArquivoTXT

---
+++ src/Carga/Carga/Interfaces/Log/IGerarArquivoTXT.cs
@@ -0,0 +1,10 @@
+﻿
+using Carga.Entities;
+
+namespace Carga.Interfaces.Log
+{
+    public interface IGerarArquivoTXT
+    {
+        bool GerarArquivo(Agendamento agendamento, string id);
+    }
+}
