0410475905b1ef05737ecf829b475fa618cfa4a1
Washington <Washington@WASHINGTON-PC>
2020-05-13 08:25:09 -0300

    Versão Final

--- src/Carga.Service/Worker.cs
+++ src/Carga.Service/Worker.cs
@@ -1,10 +1,8 @@
 using Carga.Domain.Notificacoes;
 using Carga.Repository;
 using Carga.Services.Processa;
-using Dapper;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using MySql.Data.MySqlClient;
 using System;
 using System.Linq;
 using System.Threading;
@@ -40,79 +38,51 @@ namespace Carga.Service
             {
                 try
                 {
-                    if (!IsChaveAtiva())
+
+                    var notificacao = new DomainNotificationHandler();
+                    var _iAgendamentoRepository = new AgendamentoRepository(notificacao);
+
+                    //Caso haja exceção na instância do BD, notifica erro
+                    if (notificacao.HasNotifications())
                     {
-                        Serilog.Log.Logger.Error($"Serviço expirado, entre em contato com o desenvolvedor", DateTimeOffset.Now);
+                        Serilog.Log.Logger.Error($"Worker error at: {notificacao.GetShowNotifications().FirstOrDefault()}", DateTimeOffset.Now);
+                        notificacao.Dispose();
                     }
                     else
                     {
-                        var notificacao = new DomainNotificationHandler();
-                        var _iAgendamentoRepository = new AgendamentoRepository(notificacao);
+                        var agendamentos = _iAgendamentoRepository.BuscarAgendamentos();
 
-                        //Caso haja exceção na instância do BD, notifica erro
+                        //Caso haja exceção na consulta dos agendamentos no banco, notifica erro
                         if (notificacao.HasNotifications())
                         {
                             Serilog.Log.Logger.Error($"Worker error at: {notificacao.GetShowNotifications().FirstOrDefault()}", DateTimeOffset.Now);
                             notificacao.Dispose();
                         }
-                        else
+
+                        //processa os agendamentos encontrados
+                        agendamentos?.ToList().ForEach(agendamento =>
                         {
-                            var agendamentos = _iAgendamentoRepository.BuscarAgendamentos();
-
-                            //Caso haja exceção na consulta dos agendamentos no banco, notifica erro
-                            if (notificacao.HasNotifications())
-                            {
-                                Serilog.Log.Logger.Error($"Worker error at: {notificacao.GetShowNotifications().FirstOrDefault()}", DateTimeOffset.Now);
-                                notificacao.Dispose();
-                            }
-
-                            //processa os agendamentos encontrados
-                            agendamentos?.ToList().ForEach(agendamento =>
-                            {
                                 //Start Processamento
                                 new ProcessaService(notificacao).IniciarProcessamento(agendamento);
 
                                 //Caso haja exceção na consulta dos agendamentos no banco, notifica erro
                                 if (notificacao.HasNotifications())
-                                {
-                                    Serilog.Log.Logger.Error($"Worker error at: {notificacao.GetShowNotifications().FirstOrDefault()}", DateTimeOffset.Now);
-                                    notificacao.Dispose();
-                                }
+                            {
+                                Serilog.Log.Logger.Error($"Worker error at: {notificacao.GetShowNotifications().FirstOrDefault()}", DateTimeOffset.Now);
+                                notificacao.Dispose();
+                            }
 
-                            });
+                        });
 
-                           
-                        }
-                    }                   
+
+                    }
+
                 }
                 catch (Exception ex)
                 {
                     Serilog.Log.Logger.Error($"Worker error at: {ex.Message}", DateTimeOffset.Now);
                 }
             }
-        }
-
-        public bool IsChaveAtiva()
-        {
-            using (var conn = new MySqlConnection("Server=45.132.242.97; Port=3306;Database=app;Uid=wmysql;Pwd=**n0gkzvyftwbljcemhdqi**;"))
-            {
-                try
-                {
-
-                    //carga service
-                    var dataAtual = DateTime.Now;
-                    return conn.Query<bool>("SELECT * FROM `licenca` WHERE `ChaveLicenca` = '63617267612073657276696365' AND `Ativo` = 1 AND  cast(@dataAtual as date) <= `ValidaAte`", new { dataAtual }).Any();
-                }
-                catch (System.Exception ex)
-                {
-                    return false;
-                }
-                finally
-                {
-                    conn.Close();
-                }
-            }
-
         }
     }
 }
